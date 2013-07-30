ed8819cc973d51c44caa9ffe1e3dd30c2de4bd17
Christophe Bamelis <bamelis@Q9550.(none)>
2013-07-30 19:19:50 +0200

    Replaced debian 7.0 by 7.1

--- definitions/.cfg/debian-7.1.0-minimal.rb
+++ definitions/.cfg/debian-7.1.0-minimal.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + "/../.cfg/linux_family_debian.rb"
 
-iso = "debian-7.0.0-amd64-netinst.iso"
+iso = "debian-7.1.0-amd64-netinst.iso"
 
 DEBIAN_7_MINIMAL = LINUX_FAMILY_DEBIAN.merge(
   :os_type_id => 'Debian_64',
   :iso_file => "Debian/#{iso}",
-  #:iso_src => "http://mirror.myip.be/pub/centos/6.4/isos/x86_64/#{iso}",
-  :iso_md5 => "6a55096340b5b1b7d335d5b559e13ea0",
+  #:iso_src => "http://mirror.as35701.net/debian-cd/7.1.0/amd64/iso-cd/#{iso}",
+  :iso_md5 => "80f498a1f9daa76bc911ae13692e4495"
 )
 
