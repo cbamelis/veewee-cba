75a561abd3c1186c737b73a777b8f74ca738f66c
Christophe Bamelis <christophe.bamelis@gmail.com>
2013-10-14 07:50:52 +0100

    Use full OS version in puppet URL repo (instead of major version number only)

--- definitions/.scripts/puppet.sh
+++ definitions/.scripts/puppet.sh
@@ -3,9 +3,9 @@ source common.sh
 
 ########## add puppet repo ##########
 
-rhel5   http_package_install  http://yum.puppetlabs.com/el/${OS_MAJOR_VERSION}/products/${ARCH}  puppetlabs-release-5-7.noarch.rpm
-rhel6   http_package_install  http://yum.puppetlabs.com/el/${OS_MAJOR_VERSION}/products/${ARCH}  puppetlabs-release-6-7.noarch.rpm
-debian  http_package_install  http://apt.puppetlabs.com                                          puppetlabs-release-${OS_CODE_NAME}.deb
+rhel5   http_package_install  http://yum.puppetlabs.com/el/${OS_FULL_VERSION}/products/${ARCH}  puppetlabs-release-5-7.noarch.rpm
+rhel6   http_package_install  http://yum.puppetlabs.com/el/${OS_FULL_VERSION}/products/${ARCH}  puppetlabs-release-6-7.noarch.rpm
+debian  http_package_install  http://apt.puppetlabs.com                                         puppetlabs-release-${OS_CODE_NAME}.deb
 
 ########## install puppet ##########
 
