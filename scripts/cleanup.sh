2b5174a06848b4ae56274fcc9cea0771614dd677
Christophe Bamelis <christophe.bamelis@gmail.com>
2014-12-06 14:15:02 +0100

    Merged zerodisk.sh with cleanup.sh

---
+++ scripts/cleanup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+source common.sh
+
+# remove traces of mac address from network configuration
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+rhel find /etc/sysconfig/network-scripts -name "ifcfg-eth*" -exec sed -i /HWADDR/d {} \;
+rhel find /etc/sysconfig/network-scripts -name "ifcfg-eth*" -exec sed -i /UUID/d {} \;
+
+# remove GCC and other build related packages
+#remove_packages make gcc dkms  # don't remove these packages: redhat-lsb-core is dependent on these
+#debian remove_packages linux-headers-${KERNEL_VERSION} xserver-xorg         build-essential
+#rhel   remove_packages kernel-devel-${KERNEL_VERSION}  xorg-x11-server-Xorg glibc-devel glibc-headers kernel-headers mesa-libGL
+
+# if an Oracle UEK kernel is installed: remove all other kernels
+uek rpm -qa | grep ^kernel | grep -v ^kernel-uek | xargs yum remove -y
+
+# remove packages we don't need any more
+rhel ensure_packages yum-utils
+rhel package-cleanup -y --oldkernels --count=1
+#rhel package-cleanup --leaves --exclude-bin | grep -v yum | xargs yum remove -y
+
+# clean package cache
+rhel yum -y clean all
+debian apt-get -y autoremove
+debian apt-get -y clean
+
+# delete veewee user
+VEEWEE_USER=veewee
+sudo_without_password ${VEEWEE_USER}
+rm -rf /home/${VEEWEE_USER}/*.iso
+#test -d /home/${VEEWEE_USER} && userdel -f ${VEEWEE_USER} && rm -rf /home/${VEEWEE_USER} && rm -rf /etc/sudoers.d/${VEEWEE_USER}
+#sed -i 's/^veewee.*$//' /etc/sudoers
+
+# remove not required files
+find /tmp ! -fstype vboxsf -type f -print0 | xargs -0 rm
+find /tmp ! -fstype vboxsf -type d -empty -delete
+find /var/log -iname "*.log" | xargs rm -f
+find / -iname ".bash_history" | xargs rm -f
+
+# zero out the free space to save space in the final image
+for ROOT in $(cat /proc/mounts | grep ext4 | cut -d" " -f 2); do
+    echo "Blanking free space on ${ROOT}..."
+    dd if=/dev/zero of=${ROOT}/EMPTY bs=10M
+    rm -f ${ROOT}/EMPTY
+done
+
+# find swap partition and remember UUID
+SWAPPART=`cat /proc/swaps | tail -n1 | awk '{print $1}'`
+OLD_UUID=`blkid ${SWAPPART} -s UUID -o value`
+
+# zero out swap space and re-initialize
+echo "Blanking swap space..."
+swapoff ${SWAPPART}
+dd if=/dev/zero of=${SWAPPART} bs=10M
+mkswap ${SWAPPART}
+swapon ${SWAPPART}
+
+# update swap space UUID in /etc/fstab
+NEW_UUID=`blkid ${SWAPPART} -s UUID -o value`
+#OLD_UUID=`echo "${OLD_UUID}" | sed 's/\-/\\\-/g'`
+#NEW_UUID=`echo "${NEW_UUID}" | sed 's/\-/\\\-/g'`
+UUID_UPDATE="s/${OLD_UUID}/${NEW_UUID}/"
+sed -i "${UUID_UPDATE}" /etc/fstab
+
