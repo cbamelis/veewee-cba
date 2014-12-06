7bdc09d40f62c69ea61648967e0655fafdbf0314
Christophe Bamelis <christophe.bamelis@gmail.com>
2014-12-06 14:17:24 +0100

    Relinked veewee kickstart scripts

---
+++ veewee/debian-7.1.0-amd64/definition.rb
@@ -0,0 +1,2 @@
+require File.dirname(__FILE__) + "/../.cfg/debian-7.1.0-minimal.rb"
+Veewee::Session.declare DEBIAN_71_MINIMAL
---
+++ veewee/debian-7.7.0-amd64/definition.rb
@@ -0,0 +1,2 @@
+require File.dirname(__FILE__) + "/../.cfg/debian-7.7.0-minimal.rb"
+Veewee::Session.declare DEBIAN_77_MINIMAL
