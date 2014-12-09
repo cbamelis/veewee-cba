6031aefd889fcbdbdc35a44079b5eda6b330d9d2
Christophe Bamelis <christophe.bamelis@gmail.com>
2014-12-09 19:21:47 +0100

    Generic puppet repo for EL

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -3,9 +3,8 @@ source common.sh
 
 ########## add puppet repo ##########
 
-el5     http_package_install  http://yum.puppetlabs.com/el/${OS_MAJOR_VERSION}/products/${ARCH}  puppetlabs-release-5-11.noarch.rpm
-el6     http_package_install  http://yum.puppetlabs.com/el/${OS_MAJOR_VERSION}/products/${ARCH}  puppetlabs-release-6-11.noarch.rpm
 debian  http_package_install  http://apt.puppetlabs.com                                          puppetlabs-release-${OS_CODE_NAME}.deb
+el      http_package_install  http://yum.puppetlabs.com/el/${OS_MAJOR_VERSION}/products/${ARCH}  puppetlabs-release-${OS_MAJOR_VERSION}-11.noarch.rpm
 
 ########## install puppet ##########
 
