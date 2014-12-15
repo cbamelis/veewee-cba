797164337841b584983a85ae6df39bbc3669c234
Christophe Bamelis <christophe.bamelis@gmail.com>
2014-12-15 17:56:58 +0100

    Improvements for running within jenkins

---
+++ packer-kvm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+CMD="packer build -only=qemu"
+test ! -z ${JENKINS_URL} && CMD="${CMD} -force -machine-readable -var 'HEADLESS=true'"
+${CMD} $@
---
+++ packer-vbox.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+CMD="packer build -only=virtualbox-iso"
+test ! -z ${JENKINS_URL} && CMD="${CMD} -force -machine-readable -var 'HEADLESS=true'"
+${CMD} $@
