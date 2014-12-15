19452eb6cf354b3582d79f16ea5aaabcdc137525
Christophe Bamelis <christophe.bamelis@tomtom.com>
2014-12-15 08:33:02 +0100

    Improvements

---
+++ install_rbenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+# restart shell
+rbenv rehash
+rbenv install 2.1.5
+rbenv rehash
