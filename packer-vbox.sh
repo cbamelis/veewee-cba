7a48f0301e1a5c920d721b236982e9c2629831ee
Christophe Bamelis <christophe.bamelis@gmail.com>
2014-12-15 18:19:31 +0100

    Fixed HEADLESS variable

--- packer-vbox.sh
+++ packer-vbox.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 CMD="packer build -only=virtualbox-iso"
-test ! -z ${JENKINS_URL} && CMD="${CMD} -force -machine-readable -var 'HEADLESS=true'"
+test -z ${JENKINS_URL} || CMD="${CMD} -force -machine-readable -var HEADLESS=true"
 ${CMD} $@
