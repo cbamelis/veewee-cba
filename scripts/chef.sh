27a4cd1dea0c564c424029d30cdd8b5e743a381e
Christophe Bamelis <christophe.bamelis@tomtom.com>
2014-12-16 09:17:59 +0100

    Fixed chef check

--- scripts/chef.sh
+++ scripts/chef.sh
@@ -7,4 +7,4 @@ ensure_packages wget bash
 wget https://www.opscode.com/chef/install.sh -q -O - | bash
 
 # validate by showing installed package versions
-echo -en 'Chef version : ' && chef --version
+echo -en 'Chef version : ' && chef-solo --version
