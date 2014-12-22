75b462c0113cf1f93138c942c8c1aedea2d536a6
Christophe Bamelis <christophe.bamelis@gmail.com>
2014-12-22 18:34:11 +0100

    Another attempt to fix TT check

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -3,7 +3,7 @@ source common.sh
 
 ########## add puppet repo (except for TT) ##########
 
-if [ test "(${TOMTOM})" = "()" ]; then
+if [ ! -v TOMTOM ]; then
   debian  http_package_install  http://apt.puppetlabs.com                                          puppetlabs-release-${OS_CODE_NAME}.deb
   el      http_package_install  http://yum.puppetlabs.com/el/${OS_MAJOR_VERSION}/products/${ARCH}  puppetlabs-release-${OS_MAJOR_VERSION}-11.noarch.rpm
 fi
