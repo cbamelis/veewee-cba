e3358d2687386aa86e2d30a3b0b76e682f2025b6
Christophe Bamelis <christophe.bamelis@gmail.com>
2014-12-27 11:24:46 +0100

    Vagrant add box

--- packer-vbox.sh
+++ packer-vbox.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -l
-CMD="packer build -only=virtualbox-iso"
+PROV=virtualbox
+CMD="packer build -only=${PROV}-iso"
 test -z ${JENKINS_URL} || CMD="${CMD} -force -machine-readable -var HEADLESS=true"
-${CMD} $@
+${CMD} $@ && vagrant box remove -f --provider ${PROV} && vagrant box add -f --provider ${PROV} --box-version ${BUILD_NUMBER-1} $(find ./boxes -iname "*.box")
