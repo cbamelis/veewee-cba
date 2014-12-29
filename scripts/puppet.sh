2665f54de59b6518d760e9019ace466370a98aa2
Christophe Bamelis <christophe.bamelis@gmail.com>
2014-12-29 17:48:51 +0100

    Forced puppet installation to latest version on debian

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -4,8 +4,9 @@ source common.sh
 ########## add puppet repo (except for TT) ##########
 
 if (test -z ${TOMTOM}); then
-  debian  http_package_install  http://apt.puppetlabs.com                                          puppetlabs-release-${OS_CODE_NAME}.deb
   el      http_package_install  http://yum.puppetlabs.com/el/${OS_MAJOR_VERSION}/products/${ARCH}  puppetlabs-release-${OS_MAJOR_VERSION}-11.noarch.rpm
+  debian  http_package_install  http://apt.puppetlabs.com                                          puppetlabs-release-${OS_CODE_NAME}.deb
+  debian  apt-get update
 fi
 
 ########## install puppet ##########
