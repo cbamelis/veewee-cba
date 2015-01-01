883f7f2ca556a14512f3f04e8c6b69c83291edc6
Christophe Bamelis <christophe.bamelis@gmail.com>
2015-01-01 16:45:53 +0100

    Fixed veewee kickstarts

---
+++ definitions/ubuntu-12.04-desktop-64bit/definition.rb
@@ -0,0 +1,2 @@
+require File.dirname(__FILE__) + "/../.cfg/ubuntu-12.04-desktop.rb"
+Veewee::Session.declare UBUNTU_1204_DESKTOP
---
+++ definitions/ubuntu-12.04-server-64bit/definition.rb
@@ -0,0 +1,2 @@
+require File.dirname(__FILE__) + "/../.cfg/ubuntu-12.04-server.rb"
+Veewee::Session.declare UBUNTU_1204_SERVER
