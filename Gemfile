ff74a560e30c9f8ddba1d3f66a5d53f6574004bb
Christophe Bamelis <christophe.bamelis@tomtom.com>
2015-11-24 18:32:48 +0100

    Fixed installation

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 gem "veewee", :git => 'https://github.com/jedi4ever/veewee.git', :tag => "v0.4.5.1"
+gem "net-scp"
 gem "ruby-libvirt"
 gem "veewee-to-packer"
--- install_rbenv.sh
+++ install_rbenv.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone -- https://github.com/carsomyr/rbenv-bundler.git ~/.rbenv/plugins/bundler
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 # restart shell
 rbenv rehash
-rbenv install 2.1.5
+rbenv install 2.2.1
 rbenv rehash
+gem install bundler
+bundle install
