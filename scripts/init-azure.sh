e7bd40c9c63b6cd15e1a44c476ea24e8ae420a02
Christophe Bamelis <Christophe.Bamelis@tomtom.com>
2019-05-06 16:00:45 +0200

    Moved AZ CLI installation to kickstart/preseed

--- scripts/init-azure.sh
+++ scripts/init-azure.sh
@@ -68,11 +68,9 @@ EOF
   chmod a+x /usr/bin/aws
 
   # package installation
-  #remove_packages hypervkvpd
-  #ensure_packages microsoft-hyper-v
   ensure_packages python-pyasn1 WALinuxAgent
   chkconfig waagent on
-  
+
   # reconfigure grub
   for REPLACE in "s|rhgb||g" "s|quiet||g" "s|crashkernel=auto|rootdelay=300\ console=ttyS0\ earlyprintk=ttyS0|g" ; do
     sed -i -e "${REPLACE}" /boot/grub/grub.conf
@@ -122,11 +120,6 @@ EOF
   ensure_packages cloud-init cloud-utils cloud-utils-growpart
   cloud-init init --local
   rm -rf /var/lib/cloud
-  
-  # install azure cli
-  #rpm --import https://packages.microsoft.com/keys/microsoft.asc
-  #sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
-  #ensure_packages azure-cli
 
   # install aws cli
   ensure_packages python36 python36-pip
@@ -158,7 +151,8 @@ function ubuntu_azure() {
   # - https://docs.microsoft.com/en-us/azure/virtual-machines/linux/create-upload-ubuntu
   
   # upgrade repos
-  ubuntu1604 sed -i 's/[a-z][a-z].archive.ubuntu.com/azure.archive.ubuntu.com/g' /etc/apt/sources.list
+  ubuntu1604 sed -i 's/[a-z][a-z].archive.ubuntu.com/azure.archive.ubuntu.com/g' /etc/apt/sources.list \
+  || ubuntu1804 sed -i 's/[a-z][a-z].archive.ubuntu.com/azure.archive.ubuntu.com/g' /etc/apt/sources.list
   apt-get update -q
 
   # add cloud-init support
@@ -166,13 +160,6 @@ function ubuntu_azure() {
   cloud-init init --local
   rm -rf /var/lib/cloud
   
-  # install azure cli
-  ensure_packages apt-transport-https curl wget lsb-release
-  echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/azure-cli.list
-  apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv --keyserver packages.microsoft.com --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
-  apt-get update -q
-  apt-get install --no-install-recommends -y azure-cli
-
   # install aws cli
   ensure_packages python-setuptools python-pip
   pip install awscli
@@ -182,13 +169,20 @@ function ubuntu_azure() {
   
   # package installation
   ensure_packages walinuxagent
-
+  
   # reconfigure grub
   sed -i -e 's|GRUB_CMDLINE_LINUX_DEFAULT.*|GRUB_CMDLINE_LINUX_DEFAULT="console=tty1 console=ttyS0,115200n8 earlyprintk=ttyS0,115200 rootdelay=300"|g' /etc/default/grub
   update-grub
 }
 
+
 ##### Run OS dependent Azure customizations #####
 
 el6 el6_azure || el7 el7_azure || ubuntu ubuntu_azure
 
+
+##### Show AZ CLI versions #####
+
+az --version
+which azcopy || exit -1
+azcopy --version || :
